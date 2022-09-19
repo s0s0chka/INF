8d11c18559fbd765a233b211634c61b519076991
sosochka <bsnofkz@mail.ru>
2022-09-19 17:51:10 +0300

    Added base 10 to fib

--- Lab1/InfLab.java
+++ Lab1/InfLab.java
@@ -1,30 +1,40 @@
 import java.util.Scanner;
-
 import packages.BaseConvert;
 
 public class InfLab {
 
-    public static String ConvertWithNumberBases(String NumberToConvert, int OldBase, int NewBase) {
-        if (OldBase == 10){
-            return BaseConvert.ConvertFromBaseTen(NumberToConvert, NewBase);
-        } else if (OldBase != 10 && NewBase != 10) {
-            String temp = BaseConvert.ConvertToBaseTen(NumberToConvert, OldBase);
-            return BaseConvert.ConvertFromBaseTen(temp, NewBase);
-        } else {
-            return BaseConvert.ConvertToBaseTen(NumberToConvert, OldBase);
+    public static String convertNumberBase(String numberToConvert, String oldBase, String newBase) {
+        if(newBase.equals("fib")) {
+            return BaseConvert.convertFromBaseTenToBaseFib(numberToConvert);
+        }
+        else if (oldBase.equals("10")){
+            return BaseConvert.convertFromBaseTen(numberToConvert, Integer.parseInt(newBase));
+        } else if (newBase.equals("10")) {
+            return BaseConvert.convertToBaseTen(numberToConvert, Integer.parseInt(oldBase));
+        }
+        else {
+            String temp = BaseConvert.convertToBaseTen(numberToConvert, Integer.parseInt(oldBase));
+            return BaseConvert.convertFromBaseTen(temp, Integer.parseInt(newBase));
         }
     }
 
     public static void main(String[] args) {
         Scanner scn = new Scanner(System.in);
 
-        String NumberToConvert = scn.nextLine();
-        int OldBase = scn.nextInt();
-        int NewBase = scn.nextInt();
+        String numberToConvert = scn.nextLine();
+        String oldBase = scn.nextLine();
+        String newBase = scn.nextLine();
 
         scn.close();
         
-        System.out.println(ConvertWithNumberBases(NumberToConvert, OldBase, NewBase));
+        System.out.println(convertNumberBase(numberToConvert, oldBase, newBase));
 
     }
 }
+
+/*
+ * To do: 
+ * Base -10 to 10
+ * Base 9c to 10
+ * Base 10 to fact
+ */
--- Lab1/packages/BaseConvert.java
+++ Lab1/packages/BaseConvert.java
@@ -1,88 +1,109 @@
 package packages;
 
+import java.util.*;
+
 public class BaseConvert {
     private static final String NUMBERS_AS_LETTERS = "0123456789ABCDEF";
 
-    private static String ReverseString(String StrToReverse) {
+    private static String reverseString(String stringToReverse) {
         String result = "";
-        for (int i = StrToReverse.length()-1; i >= 0; i--){
-            result += StrToReverse.charAt(i);
+        for (int i = stringToReverse.length()-1; i >= 0; i--){
+            result += stringToReverse.charAt(i);
         }
         return result;
     }
 
-    private static double FractionalPartFromString(String FractionalPart){
+    private static double getFractionalPartFromString(String fractionalPart){
         double result = 0.0;
-        for (int i = 0; i < FractionalPart.length(); i++){
-            result += Double.parseDouble(FractionalPart.substring(i, i+1)) * Math.pow(10, -(i+1));
+        for (int i = 0; i < fractionalPart.length(); i++){
+            result += Double.parseDouble(fractionalPart.substring(i, i+1)) * Math.pow(10, -(i+1));
         }
         return result;
     }
 
-    public static String ConvertFromBaseTen(String NumberToConvert, int NewBase) {
+    public static String convertFromBaseTen(String numberToConvert, int newBase) {
 
         String result = "";
 
-        if (NumberToConvert.contains(",")){ //check if fractional
+        if (numberToConvert.contains(",")){ //check if fractional
 
-            String[] parts = NumberToConvert.split(",");
+            String[] parts = numberToConvert.split(",");
 
-            int NumberToConvert_Integer = Integer.parseInt(parts[0]);
-            double NumberToConvert_Fractional = FractionalPartFromString(parts[1]);
+            int numberToConvertInteger = Integer.parseInt(parts[0]);
+            double numberToConvertFractional = getFractionalPartFromString(parts[1]);
 
-            while (NumberToConvert_Integer > 0) {
-                result += NUMBERS_AS_LETTERS.charAt(NumberToConvert_Integer % NewBase);
-                NumberToConvert_Integer /= NewBase;
+            while (numberToConvertInteger > 0) {
+                result += NUMBERS_AS_LETTERS.charAt(numberToConvertInteger % newBase);
+                numberToConvertInteger /= newBase;
             }
 
-            result = ReverseString(result) +  ",";
+            result = reverseString(result) +  ",";
 
-            while(NumberToConvert_Fractional != 0){
-                NumberToConvert_Fractional *= NewBase;
-                double FrPa = NumberToConvert_Fractional % 1;
-                double IntPa = NumberToConvert_Fractional - FrPa;
-                result += NUMBERS_AS_LETTERS.charAt((int)IntPa);
-                if (IntPa >= 1){
-                    NumberToConvert_Fractional = FrPa;
+            while(numberToConvertFractional != 0){
+                numberToConvertFractional *= newBase;
+                double fractionalPart = numberToConvertFractional % 1;
+                int integerPart = (int)(numberToConvertFractional - fractionalPart);
+                result += NUMBERS_AS_LETTERS.charAt(integerPart);
+                if (numberToConvertInteger >= 1){
+                    numberToConvertFractional = fractionalPart;
                 }
             }
         } else {
-            int NumberToConvert_Integer = Integer.parseInt(NumberToConvert);
-            while (NumberToConvert_Integer > 0) {
-                result += NUMBERS_AS_LETTERS.charAt(NumberToConvert_Integer % NewBase);
-                NumberToConvert_Integer /= NewBase;
+            int numberToConvertInteger = Integer.parseInt(numberToConvert);
+            while (numberToConvertInteger > 0) {
+                result += NUMBERS_AS_LETTERS.charAt(numberToConvertInteger % newBase);
+                numberToConvertInteger /= newBase;
             }
-            result = ReverseString(result);
+            result = reverseString(result);
         }
         return result;
     }
 
-    public static String ConvertToBaseTen(String NumberToConvert, int OldBase){
-        if (NumberToConvert.contains(",")){
+    public static String convertToBaseTen(String numberToConvert, int OldBase){
+        if (numberToConvert.contains(",")){
             double result = 0;
-            int indexO = NumberToConvert.indexOf(",");
-            int IntPartLength = NumberToConvert.substring(0, indexO).length();
-            for(int i = 0; i < IntPartLength; i++){
-                char CurrentChar = NumberToConvert.charAt(i);
-                int CharAsBaseTenNumber = NUMBERS_AS_LETTERS.indexOf(CurrentChar);
-                result += CharAsBaseTenNumber * Math.pow(OldBase, IntPartLength - i - 1);
+            int indexO = numberToConvert.indexOf(",");
+            int integerPartrtLength = numberToConvert.substring(0, indexO).length();
+            for(int i = 0; i < integerPartrtLength; i++){
+                int CharAsBaseTenNumber = NUMBERS_AS_LETTERS.indexOf(numberToConvert.charAt(i));
+                result += CharAsBaseTenNumber * Math.pow(OldBase, integerPartrtLength - i - 1);
             }
             int counter = -1;
-            for (int i = indexO + 1; i < NumberToConvert.length(); i++){
-                char CurrentChar = NumberToConvert.charAt(i);
-                int CharAsBaseTenNumber = NUMBERS_AS_LETTERS.indexOf(CurrentChar);
+            for (int i = indexO + 1; i < numberToConvert.length(); i++){
+                int CharAsBaseTenNumber = NUMBERS_AS_LETTERS.indexOf(numberToConvert.charAt(i));
                 result += CharAsBaseTenNumber * Math.pow(OldBase, counter);
                 counter--;
             }
             return (Double.toString(result)).replace(".", ",");
         } else {
             int result = 0;
-            for(int i = 0; i < NumberToConvert.length(); i++){
-                char CurrentChar = NumberToConvert.charAt(i);
-                int CharAsBaseTenNumber = NUMBERS_AS_LETTERS.indexOf(CurrentChar);
-                result += CharAsBaseTenNumber * Math.pow(OldBase, NumberToConvert.length() - i - 1);
+            for(int i = 0; i < numberToConvert.length(); i++){
+                int CharAsBaseTenNumber = NUMBERS_AS_LETTERS.indexOf(numberToConvert.charAt(i));
+                result += CharAsBaseTenNumber * Math.pow(OldBase, numberToConvert.length() - i - 1);
             }
             return Integer.toString(result);
         }
+    }
+
+    public static String convertFromBaseTenToBaseFib(String numberToConvert){
+        String result = "";
+        int numberToConvertInteger = Integer.parseInt(numberToConvert);
+        List<Integer> fib = new ArrayList<Integer>();
+        fib.add(1);
+        fib.add(2);
+        while (fib.get(fib.size() - 1) < numberToConvertInteger) {
+            fib.add(fib.get(fib.size() - 1) + fib.get(fib.size()-2));
+        }
+        fib.remove(fib.size()-1);
+        fib.sort(Comparator.reverseOrder());
+        for (int i = 0; i < fib.size(); i++){
+            if(numberToConvertInteger - fib.get(i) >= 0){
+                result += "1";
+                numberToConvertInteger -= fib.get(i);
+            } else {
+                result += "0";
+            }
+        }
+        return result;
     }
 }
